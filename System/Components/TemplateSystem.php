9427e1dd76800146ae56e5999023daa5283cbf81
Michael Miller <commnerd@gmail.com>
2018-04-20 07:12:56 -0600

    Routing completed for APF

--- System/Components/TemplateSystem.php
+++ System/Components/TemplateSystem.php
@@ -21,6 +21,6 @@ class TemplateSystem extends AppComponent
 
 	public function render($template, $params)
 	{
-		$this->_driver->render($template, $params);
+		return $this->_driver->render($template, $params);
 	}
 }
