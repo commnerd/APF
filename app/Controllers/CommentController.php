00a6c564fa294c0ee5de00667b4c5bbd77b0f41b
Michael Miller <commnerd@gmail.com>
2018-05-04 20:58:59 -0600

    Create controllers

---
+++ app/Controllers/CommentController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controllers;
+
+use System\Components\Request;
+use App\Models\Comment;
+
+class CommentController extends BaseController
+{
+    /**
+     * Generate page for creating a new post
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        return $this->view('comments/create.html');
+    }
+
+    /**
+     * Store a new Comment
+     *
+     * @param  Request $request The request and its attributes
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        Comment::create($request->toArray);
+
+        $this->redirect('back');
+    }
+
+    /**
+     * Show page for editing a comment
+     *
+     * @param  integer $id The ID of the comment to edit
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $comment = Comment::findOrFail($id);
+
+        return $this->view('comments/edit.html', array(
+            'comment' => $comment
+        ));
+    }
+
+    /**
+     * Update a comment
+     *
+     * @param  Request $request The request and its attributes
+     * @param  integer $id The ID of the comment to edit
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        $comment = Comment::findOrFail($id);
+
+        $comment->fill($request->toArray());
+
+        return $this->redirect('back');
+    }
+
+    /**
+     * Delete a comment
+     *
+     * @param  integer $id The ID of the comment to delete
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        Comment::delete($id);
+
+        return $this->redirect('back');
+    }
+}
---
+++ app/Controllers/PostController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controllers;
+
+use System\Components\Request;
+use App\Models\Post;
+
+class PostController extends BaseController
+{
+    /**
+     * Creation page for a new post
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        return $this->view('posts/create.html');
+    }
+
+    /**
+     * Store a new Post
+     *
+     * @param  Request $request The request and its attributes
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        Post::create($request->toArray());
+
+        return $this->redirect('back');
+    }
+
+    /**
+     * Show page for editing a post
+     *
+     * @param  integer $id The ID of the comment to edit
+     * @return Response
+     */
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+
+        return $this->view('posts/show.html', array(
+            'post' => $post
+        ));
+    }
+
+    /**
+     * Edit a post
+     *
+     * @param  integer $id The ID of the post to edit
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+
+        return $this->view('posts/edit.html', array(
+            'post' => $post
+        ));
+    }
+
+    /**
+     * Update a post
+     *
+     * @param  Request $request The request and its attributes
+     * @param  integer $id The ID of the post to edit
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        $post = Post::findOrFail($id);
+
+        $post->fill($request->toArray());
+
+        return $this->redirect('back');
+    }
+
+    /**
+     * Delete a post
+     *
+     * @param  integer $id The ID of the post to edit
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        Post::delete($id);
+
+        return $this->redirect('back');
+    }
+}
