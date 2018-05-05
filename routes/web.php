062fc90a080fb6006ab4ed614b5e494f0168ef51
Michael Miller <commnerd@gmail.com>
2018-05-04 22:17:45 -0600

    Create routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 return array(
 	array('GET', '/', '\App\Controllers\WelcomeController#index', 'home'),
+	array('RESOURCE', 'posts', '\App\Controllers\PostController'),
+	array('RESOURCE', 'comments', '\App\Controllers\CommentController'),
 );
 
 ?>
